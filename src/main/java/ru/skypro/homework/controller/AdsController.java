4d845c5a3fac56098fa16b10413ff7dfced6e898
kodarov_salavat <kodarov@gmail.com>
2023-11-18 03:42:02 +0400

    fix AdsController

--- src/main/java/ru/skypro/homework/controller/AdsController.java
+++ src/main/java/ru/skypro/homework/controller/AdsController.java
@@ -7,10 +7,12 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import ru.skypro.homework.dto.AdCreateOrUpdate;
 import ru.skypro.homework.dto.AdDto;
 import ru.skypro.homework.dto.AdInfo;
 import ru.skypro.homework.dto.AdsAll;
-import ru.skypro.homework.dto.AdCreateOrUpdate;
+
+import java.io.IOException;
 
 @Slf4j
 @CrossOrigin(value = "http://localhost:3000")
@@ -27,40 +29,42 @@ public class AdsController {
      * @return
      */
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<AdDto> addAd(@RequestPart("image") MultipartFile image, @RequestPart AdDto ad) {
-        log.info("ad added");
+    public ResponseEntity<AdDto> addAd(@RequestPart("image") MultipartFile image, @RequestPart AdDto ad) throws IOException {
         return ResponseEntity.status(HttpStatus.CREATED).body(new AdDto());
     }
 
     /**
      * Получение информации об объявлении
+     *
      * @param id
      * @return
      */
     @GetMapping(value = "/{id}")
-    public AdInfo getAd(@PathVariable Integer id){
+    public AdInfo getAd(@PathVariable Integer id) {
         return new AdInfo();
     }
 
     /**
      * Обновление информации об объявлении
+     *
      * @param id
      * @param adCreateOrUpdate
      * @return
      */
     @PatchMapping(value = "/{id}")
     public ResponseEntity<AdDto> updateAd(@PathVariable Integer id,
-                                                     @RequestBody AdCreateOrUpdate adCreateOrUpdate){
+                                          @RequestBody AdCreateOrUpdate adCreateOrUpdate) {
         return ResponseEntity.ok(new AdDto());
     }
 
     /**
      * Удаление объявления
+     *
      * @param id
      * @return
      */
     @DeleteMapping(value = "/{id}")
-    public ResponseEntity<String> deleteAd(@PathVariable Integer id){
+    public ResponseEntity<String> deleteAd(@PathVariable Integer id) {
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
@@ -71,6 +75,34 @@ public class AdsController {
      */
     @GetMapping
     public ResponseEntity<AdsAll> getAllAds() {
-        return ResponseEntity.ok().body(new AdsAll());
+        return ResponseEntity.ok(new AdsAll());
+    }
+
+    /**
+     * Получение объявлений авторизованного пользователя
+     *
+     * @return
+     */
+    @GetMapping(value = "/me")
+    public ResponseEntity<AdsAll> getAdsMe() {
+        return ResponseEntity.ok(new AdsAll());
+    }
+
+    /**
+     * Обновление картинки объявления
+     * @param id
+     * @param image
+     * @return
+     * @throws IOException
+     */
+    @PatchMapping(value = "/{id}/image", consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
+            produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+    public ResponseEntity<byte[]> updateImage(@PathVariable Integer id, @RequestPart("image") MultipartFile image) throws IOException {
+        try {
+            return ResponseEntity.ok(image.getBytes()); //заглушка
+        } catch (IOException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+
     }
 }
