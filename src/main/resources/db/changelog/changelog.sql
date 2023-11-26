2cee88be4f43b59006329ed6181e0957340c52d7
kodarov_salavat <kodarov@gmail.com>
2023-11-27 01:32:45 +0400

    fix связь UserEntity с Avatar

--- src/main/resources/db/changelog/changelog.sql
+++ src/main/resources/db/changelog/changelog.sql
@@ -9,30 +9,30 @@ INSERT INTO roles (role)
 VALUES ('USER'),
        ('ADMIN');
 
---changeset 2 kodarov_s: users
+--changeset 2 kodarov_s: avatar
+CREATE TABLE avatars
+(
+    id   SERIAL PRIMARY KEY,
+    data BYTEA CHECK (LENGTH(data) <= 5 * 1024 * 1024)
+);
+
+--changeset 3 kodarov_s: users
 CREATE TABLE users
 (
     id         SERIAL PRIMARY KEY,
-    login      VARCHAR(50) NOT NULL UNIQUE,
+    login      VARCHAR(50)  NOT NULL UNIQUE,
     password   VARCHAR(255) NOT NULL,
     first_name VARCHAR(50),
     last_name  VARCHAR(50),
     phone      VARCHAR(50),
-    role       VARCHAR(50) NOT NULL
-);
---changeset 3 kodarov_s: avatar
-CREATE TABLE avatars
-(
-    id      SERIAL PRIMARY KEY,
-    data    BYTEA CHECK (LENGTH(data) <= 5 * 1024 * 1024),
-    user_id SERIAL,
-    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE -- удалим вместе с userEntity и avatar
+    role       VARCHAR(50)  NOT NULL,
+    avatar_id  SERIAL REFERENCES avatars (id) ON DELETE CASCADE
 );
 --changeset 4 kodarov_s: ad_image
 CREATE TABLE ad_image
 (
-    id    SERIAL PRIMARY KEY,
-    data  BYTEA CHECK (LENGTH(data) <= 10 * 1024 * 1024)
+    id   SERIAL PRIMARY KEY,
+    data BYTEA CHECK (LENGTH(data) <= 10 * 1024 * 1024)
     --ad_id SERIAL NOT NULL,
     --FOREIGN KEY (ad_id) REFERENCES ads (id) ON DELETE CASCADE -- удалим вместе с ad все его ad_image
 );
